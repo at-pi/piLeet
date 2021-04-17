64ab69c2502efef5e722fd13caf7bf934f50121e
Aditya Tiwari <31739160+at-pi@users.noreply.github.com>
2021-04-17 19:52:08 +0530

    Time: 1 ms (67.56%), Space: 36.8 MB (28.22%) - LeetHub

--- sum-of-unique-elements/sum-of-unique-elements.java
+++ sum-of-unique-elements/sum-of-unique-elements.java
@@ -1,24 +1,20 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
-        Map<Integer,int[]> map = new HashMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
-                int[] arr = map.get(nums[i]);
-                arr[1]++;
-                map.put(nums[i],arr);
+                int x = map.get(nums[i]);
+                x++;
+                map.put(nums[i],x);
             }
             else{
-                int[] arr = new int[2];
-                arr[0] = nums[i];
-                arr[1] = 1;
-                map.put(nums[i],arr);
+                map.put(nums[i],1);
             }
         }
         int sum=0;
-        for(int[] arr : map.values()){
-            if(arr[1]==1){
-                sum+=arr[0];
-            }
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            if(e.getValue()==1)
+                sum+=e.getKey();
         }
         return sum;
     }
