ee5fa37f889f4ad7cf52d8691443cc257c9545a7
Aditya Tiwari <31739160+at-pi@users.noreply.github.com>
2021-04-25 12:35:45 +0530

    Time: 13 ms (94.59%), Space: 48.6 MB (94.50%) - LeetHub

--- maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,31 +1,15 @@
 class Solution {
-    public int maxArea(int h, int w, int[] ha, int[] va) {
-        Arrays.sort(ha);
-        Arrays.sort(va);
-        long maxh = ha[0];
-        //System.out.println(maxh);
-        long maxv = va[0];
-        // int diffh = 0;
-        // int diffv = 0;
-        
-        for(int i=1;i<ha.length;i++){
-            System.out.println(maxh);
-            if(ha[i]-ha[i-1]>maxh){
-                maxh=ha[i]-ha[i-1];
-            }
+    public int maxArea(int h, int w, int[] hc, int[] vc) {
+        return (int) ((getMaxDist(h, hc) * getMaxDist(w, vc)) % (Math.pow(10, 9) + 7));
+    }
+    
+    private long getMaxDist(int end, int[] cuts) {
+        Arrays.sort(cuts);
+        long res = 0, from = 0;
+        for (int c : cuts) {
+            res = Math.max(res, c - from);
+            from = c;
         }
-        if(h-ha[ha.length-1]>maxh)
-            maxh=h-ha[ha.length-1];
-        // System.out.println(maxh);
-        for(int i=1;i<va.length;i++){
-            if(va[i]-va[i-1]>maxv){
-                maxv=va[i]-va[i-1];
-            }
-        }
-        if(w-va[va.length-1]>maxv)
-            maxv=w-va[va.length-1];
-        //System.out.println(maxh+" "+maxv);
-        long x = (maxh*maxv)%1000000007;
-        return (int)x;
+        return Math.max(res, end - from);
     }
 }
