fff744038faf1df03560ef23de4a6f456c394d39
Aditya Tiwari <31739160+at-pi@users.noreply.github.com>
2021-05-19 23:15:24 +0530

    Time: 0 ms (100.00%), Space: 37.9 MB (99.56%) - LeetHub

---
+++ search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    private boolean bs(int[] arr, int target, int left, int right){
+        if(left>right){
+            return false;
+        }
+        int mid = (left+right)/2;
+        if(arr[mid]==target)
+            return true;
+        else if(target>arr[mid]){
+            return bs(arr,target,mid+1,right);
+        }
+        else{
+            return bs(arr,target,left,mid-1);
+        }
+    }
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0;i<m;i++){
+            if(target>=matrix[i][0] && target<=matrix[i][n-1]){
+                return bs(matrix[i],target,0,n-1);
+            }
+        }
+        return false;
+    }
+}
