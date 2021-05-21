eb05e694c371cd93aa567183b3ef80b3180a8cda
Aditya Tiwari <31739160+at-pi@users.noreply.github.com>
2021-05-22 01:19:42 +0530

    Time: 1 ms (31.32%), Space: 39.4 MB (22.04%) - LeetHub

---
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    private void inorder(TreeNode root){
+        if(root==null)
+            return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root);
+        return list.get(k-1);
+    }
+}
