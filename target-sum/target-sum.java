cb7c9808f62273449360f85f73eb2ff6f800f029
Aditya Tiwari <31739160+at-pi@users.noreply.github.com>
2021-06-22 14:28:24 +0530

    Time: 528 ms (11.38%), Space: 36.5 MB (83.44%) - LeetHub

--- target-sum/target-sum.java
+++ target-sum/target-sum.java
@@ -1,16 +1,15 @@
 class Solution {
-    int global = 0;
-    private void util(int[] arr, int curr,int currsum,int target){
+    //int global = 0;
+    private int util(int[] arr, int curr,int currsum,int target){
         if(curr==arr.length){
             if(currsum==target)
-                global++;
-            return;
+                return 1;
+            return 0;
         }
-        util(arr,curr+1,currsum+(arr[curr]),target);
-        util(arr,curr+1,currsum+(-1*arr[curr]),target);
+        return util(arr,curr+1,currsum+(arr[curr]),target)+util(arr,curr+1,currsum+(-1*arr[curr]),target);
     }
     public int findTargetSumWays(int[] nums, int target) {
-        util(nums,0,0,target);
-        return global;
+        return util(nums,0,0,target);
+        //return global;
     }
 }
