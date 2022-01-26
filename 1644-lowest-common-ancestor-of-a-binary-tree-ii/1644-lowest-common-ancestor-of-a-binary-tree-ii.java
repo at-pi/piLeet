58a4d0c2d32a7f113cc15d75391be0974854a689
Aditya Tiwari <31739160+at-pi@users.noreply.github.com>
2022-01-27 00:27:05 +0530

    Time: 21 ms (26.39%), Space: 67.8 MB (45.24%) - LeetHub

---
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    boolean pFound;
+    boolean qFound;
+    
+    private void inorder(TreeNode root, TreeNode p, TreeNode q){
+        
+        if(root == null)
+            return;
+        
+        if(root == p)
+            this.pFound = true;
+        if(root == q)
+            this.qFound = true;
+        
+        inorder(root.left, p, q);
+        inorder(root.right, p, q);
+    }    
+    
+    private TreeNode lca(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null)
+            return null;
+        
+        if(root == p || root == q){
+            return root;
+        }
+        
+        TreeNode left = lca(root.left, p, q);
+        TreeNode right = lca(root.right, p, q);
+        
+        if(left == null && right != null)
+            return right;
+        
+        if(left != null && right == null)
+            return left;
+        
+        if(left != null && right != null)
+            return root;
+        
+        return null;
+    }
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        this.pFound = false;
+        this.qFound = false;
+        
+        inorder(root, p, q);
+        
+        TreeNode res = lca(root, p, q);
+        
+        if(pFound && qFound)
+            return res;
+        else
+            return null;
+    }
+}
